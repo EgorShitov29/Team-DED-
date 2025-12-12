48fba6c8c7b2fddbf494b6f573ca9a7f9c10b356
EgorShitov29 <137079717+EgorShitov29@users.noreply.github.com>
2025-12-13 02:37:29 +0300

    Add files via upload

--- Model/state_check/src/event_listeners.py
+++ Model/state_check/src/event_listeners.py
@@ -14,24 +14,25 @@ def check_clicable_event_button(frame: cv.typing.MatLike, event_type: str) -> bo
     тогда выводим, что можно жать на кнопку
     """
     # И кадр, и шаблон - в градацию серого
-    flag = False
+    template = cv.imread(f'../static/{event_type}_template.png', cv.IMREAD_GRAYSCALE)
+    if template is None:
+        return False
+    img_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    im_h = img_gray.shape[0]
     center = im_h // 2
     percent = round(im_h * 0.2)
-    img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    template = cv.imread(f'../static/{event_type}_template.png', cv.IMREAD_GRAYSCALE)
     w, h = template.shape[::-1]
 
     # Методы поиска
     methods = ['TM_CCOEFF', 'TM_CCOEFF_NORMED',
-                'TM_CCORR_NORMED', 'TM_SQDIFF', 'TM_SQDIFF_NORMED']
+               'TM_CCORR_NORMED', 'TM_SQDIFF', 'TM_SQDIFF_NORMED']
 
     # Используя все методы поиска, ищем шаблон на кадре
     for meth in methods:
-        img = img2.copy()
-        im_h = img.shape[::-1][1]
+        img = img_gray.copy()
         method = getattr(cv, meth)
 
-        res = cv.matchTemplate(img,template,method)
+        res = cv.matchTemplate(img, template, method)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
         if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
@@ -39,8 +40,9 @@ def check_clicable_event_button(frame: cv.typing.MatLike, event_type: str) -> bo
         else:
             top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
-        
+
     # Смотрим на положение шаблона в кадре. Если удовлетворяет условию, то вернет True
-    if all((center+percent) > tup[-1] > (center-percent) for tup in [bottom_right, top_left]):
-        flag = True
-    return flag
+    if all((center + percent) > y > (center - percent) for y in (top_left[1], bottom_right[1])):
+        return True
+
+    return False
